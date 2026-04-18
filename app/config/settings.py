5b08d6dfececafa6cb0595c2af5be724133791f2
theleerise <elieserdce@gmail.com>
2026-04-18 13:18:52 +0200

    Mod: default settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -155,9 +155,9 @@ ASGI_APPLICATION = "config.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "noltify"),
-        "USER": os.getenv("DB_USER", "noltify_app_user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "88908890"),
+        "NAME": os.getenv("DB_NAME", "postgres"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "123456"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
